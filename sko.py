52e3fea1e8c5bedb6003e5142b93e1a08a71d70a
edubay <edubay@cloudbees.com>
2020-02-20 10:48:14 -0800

    added environment key (line 22)

--- sko.py
+++ sko.py
@@ -19,7 +19,7 @@ flags = Flags()
 Rox.register("default", flags)
 
 # Setup the Rollout environment key
-cancel_event = Rox.setup("<rollout_env_key>").result();
+cancel_event = Rox.setup("5e4ed3ccff09bf25260ddf76").result();
 
 # Boolean flag example
 print('enableTutorial is %s' % flags.enableTutorial.is_enabled())
